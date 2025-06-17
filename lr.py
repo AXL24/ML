4089f663b2c33a75a65b90c533304b9b560b591b
AXL24 <110558289+AXL24@users.noreply.github.com>
2025-06-17 16:51:36 +0700

    Update lr.py

--- lr.py
+++ lr.py
@@ -7,7 +7,7 @@ def train_logistic_regression(filtered_X, filtered_y):
     filtered_y = (filtered_y >= 6).astype(int)  # Binary classification: 1 = high quality
     X_train, X_test, y_train, y_test = train_test_split(filtered_X, filtered_y, test_size=0.2, random_state=42, shuffle=True)
 
-    clf = LogisticRegression(class_weight='balanced',max_iter=1000)
+    clf = LogisticRegression()
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     report = classification_report(y_test, y_pred, output_dict=True)
