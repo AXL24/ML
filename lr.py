aea2f45b0d80b3d96b31cca0bea2bd78a3173dca
AXL24 <110558289+AXL24@users.noreply.github.com>
2025-06-17 17:22:06 +0700

    Update lr.py

--- lr.py
+++ lr.py
@@ -4,7 +4,7 @@ from sklearn.metrics import classification_report
 # Example:
 def train_logistic_regression(filtered_X, filtered_y):
     
-    filtered_y = (filtered_y >= 6).astype(int)  # Binary classification: 1 = high quality
+    filtered_y = (filtered_y >= 7).astype(int)  # Binary classification: 1 = high quality
     X_train, X_test, y_train, y_test = train_test_split(filtered_X, filtered_y, test_size=0.2, random_state=42, shuffle=True)
 
     clf = LogisticRegression()
